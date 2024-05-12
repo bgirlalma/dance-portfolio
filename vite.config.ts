19b196ca91893dbeebd92838973bb5b1fc7b8c36
Alina <akulina.alina1999gmail.com>
2024-05-12 20:18:23 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      src: '/src',
-      components: '/scr/components',
-      pages: '/src/pages'
-    }
+      src: "./src",
+      components: "./src/components",
+      pages: "./src/pages",
+    },
   },
-  base: '/dance-portfolio/'
-})
+  base: "/dance-portfolio/",
+});
