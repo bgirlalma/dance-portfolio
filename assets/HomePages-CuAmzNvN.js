15571721537266f257b3dce91c6387ad0c273027
Alina <akulina.alina1999gmail.com>
2024-05-28 15:19:20 +0300

    Updates

---
+++ assets/HomePages-CuAmzNvN.js
@@ -0,0 +1,163 @@
+import{j as e}from"./index-BXyAVk2P.js";import{p as i,G as x}from"./iconBase-BeAM9bcL.js";const m=i.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: stretch;
+    flex-wrap: wrap;
+  }
+
+  @media screen and (min-width: 1000px) {
+    display: grid;
+    grid-template-columns: 350px 1fr;
+    align-items: flex-start;
+    gap: 20px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    justify-content: center;
+    margin-right: auto;
+    margin-left: auto;
+  }
+`,h=i.div`
+  margin-bottom: 20px;
+  @-webkit-keyframes slide-down {
+    0% {
+      -webkit-transform: translateY(-100%);
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-down {
+    0% {
+      -webkit-transform: translateY(-100%);
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  animation: slide-down 1s ease-out forwards;
+
+  @media screen and (min-width: 1000px) {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+`,p=i.img`
+  width: 250px;
+  height: auto;
+
+  @media screen and (min-width: 1000px) {
+ width: auto;
+  }
+`,f=i.h1`
+  font-family: var(--main-font-family);
+  color: var(--color-main-title);
+  font-size: 40px;
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 1000px) {
+    margin-bottom: 40px;
+  }
+`,j=i.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  @media screen and (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`,o=i.li`
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 10px;
+  padding: 15px 20px;
+  gap: 10px;
+  background: rgb(80, 164, 54);
+  background: linear-gradient(
+    24deg,
+    rgba(80, 164, 54, 1) 34%,
+    rgba(101, 189, 242, 1) 81%
+  );
+
+  @-webkit-keyframes scale-up-hor-center {
+    0% {
+      -webkit-transform: scaleX(0.4);
+      transform: scaleX(0.4);
+    }
+    100% {
+      -webkit-transform: scaleX(1);
+      transform: scaleX(1);
+    }
+  }
+  @keyframes scale-up-hor-center {
+    0% {
+      -webkit-transform: scaleX(0.4);
+      transform: scaleX(0.4);
+    }
+    100% {
+      -webkit-transform: scaleX(1);
+      transform: scaleX(1);
+    }
+  }
+
+  animation: scale-up-hor-center 1s ease-in-out forwards;
+
+  @media screen and (min-width: 1200px) {
+    padding: 15px;
+  }
+`,c=i.h2`
+  font-family: var(--main-font-family);
+  color: var(--color-white);
+  font-size: 24px;
+
+  @media screen and (min-width: 1200px) {
+    font-size: 20px;
+  }
+`,l=i.h3`
+  font-family: var(--main-font-family);
+  color: var(--color-city-title);
+  font-size: 18px;
+`,r=i.p`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: var(--main-font-family);
+  color: var(--color-desc);
+`,t=i.span`
+  color: var(--color-desc);
+  font-size: 12px;
+  text-decoration: underline;
+`,s=i.p`
+  display: flex;
+  align-items: center;
+  color: var(--color-desc);
+`,a=i.li`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;function n(d){return x({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{d:"M8 3l5 5-5 5-5-5 5-5z"},child:[]}]})(d)}const g="/dance-portfolio/assets/photo-alma-Bk3guiv_.png",u=()=>e.jsxs(m,{children:[e.jsxs(h,{children:[e.jsx(f,{children:"Akulina Alina"}),e.jsx(p,{src:g,alt:"photo Alina"})]}),e.jsxs(j,{children:[e.jsxs(o,{children:[e.jsx(c,{children:"NickName"}),e.jsx(s,{children:"B.girl Alma"})]}),e.jsxs(o,{children:[e.jsx(c,{children:"Teaching Experience"}),e.jsx(s,{children:"7 years"})]}),e.jsxs(o,{children:[e.jsx(c,{children:"Styles"}),e.jsxs(s,{children:[e.jsx(n,{}),"Breaking"]}),e.jsxs(s,{children:[e.jsx(n,{}),"Hip-Hop"]}),e.jsxs(s,{children:[e.jsx(n,{}),"Acrobatics"]})]}),e.jsxs(o,{children:[e.jsx(c,{children:"Introduction to other styles"}),e.jsxs(s,{children:[e.jsx(n,{}),"Locking"]}),e.jsxs(s,{children:[e.jsx(n,{}),"House"]}),e.jsxs(s,{children:[e.jsx(n,{}),"High Heels"]}),e.jsxs(s,{children:[e.jsx(n,{}),"Jazz Funk"]})]}),e.jsxs(o,{children:[e.jsx(c,{children:"Own Dance Experience"}),e.jsxs(s,{children:[e.jsx(n,{}),"Breaking: 16 years"]}),e.jsxs(s,{children:[e.jsx(n,{}),"Hip-Hop: 4 years"]}),e.jsxs(s,{children:[e.jsx(n,{}),"Acrobatics: 2 years"]})]}),e.jsxs(o,{children:[e.jsx(c,{children:"Job"}),e.jsxs("ul",{children:[e.jsx(l,{children:"The city of Kryvyi Rih"}),e.jsx(a,{children:e.jsxs(r,{children:["Power Dance studio ",e.jsx(t,{children:"2013-2014"})]})}),e.jsx(a,{children:e.jsxs(r,{children:["White on Black ",e.jsx(t,{children:"2018-2020"})]})}),e.jsx(a,{children:e.jsxs(r,{children:['Цдют "Сонях" ',e.jsx(t,{children:"2020-2021"})]})})]}),e.jsxs("ul",{children:[e.jsx(l,{children:"The city of Kyiv"}),e.jsx(a,{children:e.jsxs(r,{children:["D.side dance studio ",e.jsx(t,{children:"from 2021"})]})}),e.jsx(a,{children:e.jsxs(r,{children:["Yunia ",e.jsx(t,{children:"from 2021"})]})}),e.jsx(a,{children:e.jsxs(r,{children:['ЦРДММП "Бригантина" ',e.jsx(t,{children:"from 2022"})]})}),e.jsx(a,{children:e.jsxs(r,{children:["Ace School ",e.jsx(t,{children:"from 2022"})]})})]})]})]})]}),y=()=>e.jsx("div",{children:e.jsx(u,{})}),w=i.div`
+  padding: 40px 20px;
+
+  background: rgb(109, 196, 122);
+  background: radial-gradient(
+    circle,
+    rgba(109, 196, 122, 1) 34%,
+    rgba(197, 213, 147, 1) 53%,
+    rgba(194, 232, 233, 1) 81%
+  );
+`,v=()=>e.jsx(w,{children:e.jsx(y,{})});export{v as default};
