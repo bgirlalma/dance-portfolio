2413d2da40218c01df8989ba2c2055c5b6330679
Alina <akulina.alina1999gmail.com>
2024-06-18 12:45:10 +0300

    so-so video page/ problem install video(have mistackes in the console log)

--- src/components/Video/videoItems/videoItems.styled.tsx
+++ src/components/Video/videoItems/videoItems.styled.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export const WrappContainer = styled.ul`
-display: flex;
-flex-wrap: wrap;
-gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+
+  @media screen and (min-width: 1200px) and (max-width: 1359px) {
+    display: flex;
+    justify-content: center;
+  }
 `;
